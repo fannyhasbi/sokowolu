8b0577691acbb8d45c831249ba237346d627efbc
fannyhasbi <fnyhsbi@gmail.com>
2018-09-26 02:11:14 +0700

    Add short info to dashboard

--- apz/views/editor/dashboard.php
+++ apz/views/editor/dashboard.php
@@ -62,7 +62,56 @@
       <!-- Content -->
       <div class="content">
         <div class="container-fluid">
-          <h2>Hello Sokowolu!</h2>
+          <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-warning card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">visibility</i>
+                  </div>
+                  <p class="card-category">Kunjungan</p>
+                  <h3 class="card-title">180</h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons">update</i> Baru Saja
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-success card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">file_copy</i>
+                  </div>
+                  <p class="card-category">Artikel</p>
+                  <h3 class="card-title">13</h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons">date_range</i> 25 Juli 2018
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-info card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">plus_one</i>
+                  </div>
+                  <p class="card-category">Artikel Dilihat</p>
+                  <h3 class="card-title">+245</h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons">update</i> Baru Saja
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <!-- End Content -->
