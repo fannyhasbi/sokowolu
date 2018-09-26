8b5d79ef4b34a4037a5d754214e94575b302d27e
fannyhasbi <fnyhsbi@gmail.com>
2018-09-27 02:37:32 +0700

    Just accepted type that can make icon

--- apz/helpers/haz_helper.php
+++ apz/helpers/haz_helper.php
@@ -1,7 +1,16 @@
 <?php
 
-function notify($message, $type = 'success', $redirect_to = null){
+function notify($message, $type = '', $redirect_to = null){
   $ci =& get_instance();
+
+  // Based on the SweetAlert documentation
+  $accepted_type = ['warning', 'error', 'success', 'info'];
+
+  // Change to empty string when the sent type isn't in the accepted_type array
+  if($type != ''){
+    if(!in_array($type, $accepted_type))
+      $type = '';
+  }
 
   $ci->session->set_flashdata('msg', $message);
   $ci->session->set_flashdata('type', $type);
